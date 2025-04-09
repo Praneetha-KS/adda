3a7d2f10e648cc2b3508204046e516b275b2f76d
Praneetha-KS <praneethapoornaks@gmail.com>
2025-04-09 19:07:28 +0530

    Create Teacher.java

---
+++ Teacher.java
@@ -0,0 +1,14 @@
+public class Teacher extends Human {
+    
+    public Teacher(int legs,int hands,int head){
+        super(legs,hands,head);     
+    }
+    
+    void teach(){
+        System.out.println("Hiii");
+    }
+    
+    void mentor(){
+        System.out.println("Byee");
+    }
+}
