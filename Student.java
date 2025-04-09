64828ccdbda324db1916f124d6088ad754b59b40
Praneetha-KS <praneethapoornaks@gmail.com>
2025-04-09 19:07:04 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,10 @@
+public class Student extends Human {
+    
+    public Student(int legs,int hands,int head){
+        super(legs,hands,head);     
+    }
+    void study(){
+         System.out.println("I am studying");
+    }
+    
+}
