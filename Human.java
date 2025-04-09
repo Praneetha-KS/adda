9c22bde0e2b0f863d3f357cc5551880113268999
Praneetha-KS <praneethapoornaks@gmail.com>
2025-04-09 19:06:39 +0530

    Create Human.java

---
+++ Human.java
@@ -0,0 +1,16 @@
+public class Human {
+    public int legs;
+    public int hands;
+    public int head;
+    
+    public Human(int legs,int hands,int head){
+        this.legs=legs;
+        this.hands=hands;
+        this.head=head;
+    }
+    
+    void think(){
+        System.out.println("I am thinking");
+    }
+}
+
